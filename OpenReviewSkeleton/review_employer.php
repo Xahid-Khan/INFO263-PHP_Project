0c4f10f905002f62e6789f506d8b2a53fc5b3588
Chenthi Heer <che138@uclive.ac.nz>
2022-09-23 15:23:49 +1200

    added missing review_employer page

---
+++ OpenReviewSkeleton/review_employer.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Review Employer</title>
+</head>
+<body>
+<h1>Review Employer</h1>
+<?php
+
+?>
+</body>
+</html>
+
